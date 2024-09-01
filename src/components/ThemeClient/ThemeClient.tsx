7bda2511c996c83d058d79f5b7b602817005766a
DanChae03 <118027673+DanChae03@users.noreply.github.com>
2024-09-01 20:23:12 +1200

    refactor: clean files and add separate theme file

--- src/components/ThemeClient/ThemeClient.tsx
+++ src/components/ThemeClient/ThemeClient.tsx
@@ -1,33 +1,8 @@
 "use client";
 
-import { createTheme, Theme, ThemeProvider } from "@mui/material/styles";
+import { theme } from "@/utils/theme";
+import { ThemeProvider } from "@mui/material/styles";
 import { ReactNode } from "react";
-import { Nunito_Sans } from "next/font/google";
-
-const font = Nunito_Sans({ subsets: ["latin"] });
-
-const theme: Theme = createTheme({
-  typography: {
-    fontFamily: font.style.fontFamily,
-  },
-  palette: {
-    primary: {
-      main: "#E51937",
-      dark: "#B5142C",
-    },
-    secondary: {
-      main: "#7E7E7E",
-      dark: "#000000",
-    },
-    background: {
-      default: "#F5F6FA",
-      paper: "#FFFFFF",
-    },
-    error: {
-      main: "#4218C9",
-    },
-  },
-});
 
 interface ThemeClientProps {
   children: ReactNode;
---
+++ src/utils/theme.ts
@@ -0,0 +1,27 @@
+import { createTheme, Theme } from "@mui/material/styles";
+import { Nunito_Sans } from "next/font/google";
+
+const font = Nunito_Sans({ subsets: ["latin"] });
+
+export const theme: Theme = createTheme({
+  typography: {
+    fontFamily: font.style.fontFamily,
+  },
+  palette: {
+    primary: {
+      main: "#E51937",
+      dark: "#B5142C",
+    },
+    secondary: {
+      main: "#7E7E7E",
+      dark: "#000000",
+    },
+    background: {
+      default: "#F5F6FA",
+      paper: "#FFFFFF",
+    },
+    error: {
+      main: "#4218C9",
+    },
+  },
+});
