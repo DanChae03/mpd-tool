7ff3019ffdadfebef87be90910d49f87eefc4e5b
DanChae03 <118027673+DanChae03@users.noreply.github.com>
2024-09-02 15:11:08 +1200

    feat: settings page UI

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import Stack from "@mui/material/Stack";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Navbar } from "@/components/Navbar";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-export default function Settings(): ReactElement {
+export default function Dashboard(): ReactElement {
+  const [link, setLink] = useState<string>("");
+  const [template, setTemplate] = useState<string>("");
+
   return (
     <Stack direction="row" height="100vh">
       <Link
@@ -20,7 +28,54 @@ export default function Settings(): ReactElement {
         height="100%"
         width="calc(100vw - 270px)"
         bgcolor="background.default"
-      ></Stack>
+        padding="63px"
+      >
+        <Typography
+          variant="h3"
+          fontWeight="bold"
+          color="primary.main"
+          paddingBottom="18px"
+        >
+          Settings
+        </Typography>
+        <Stack spacing="9px" width="100%">
+          <Typography variant="h5">Link to Support Raising Page:</Typography>
+          <TextField
+            sx={{ paddingBottom: "18px" }}
+            placeholder="https://give.studentlife.org.nz/appeals/project-year-your-name"
+            value={link}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setLink(event.target.value);
+            }}
+          />
+          <Typography variant="h5">
+            Default Email Text (This will pre-fill the draft of any emails you
+            send).
+          </Typography>
+          <TextField
+            multiline
+            rows="12"
+            value={template}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setTemplate(event.target.value);
+            }}
+            placeholder="Hello! I would like your support for my upcoming mission trip..."
+            sx={{ paddingBottom: "18px" }}
+          />
+          <Button
+            variant="contained"
+            sx={{
+              textTransform: "none",
+              fontSize: "18px",
+              height: "54px",
+              width: "216px",
+              borderRadius: "36px",
+            }}
+          >
+            Save Changes
+          </Button>
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
