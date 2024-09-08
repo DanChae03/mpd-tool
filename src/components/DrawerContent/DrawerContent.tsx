c2a05a8c0ece31fbdc7088e4c8fc4db3d32227b7
DanChae03 <118027673+DanChae03@users.noreply.github.com>
2024-09-09 10:04:18 +1200

    fix: wrong save disable condition

--- src/components/DrawerContent/DrawerContent.tsx
+++ src/components/DrawerContent/DrawerContent.tsx
@@ -423,7 +423,7 @@ export function DrawerContent({
             Exit
           </Button>
           <Button
-            disabled={disabled || name != null}
+            disabled={disabled || name == null}
             variant="contained"
             sx={{
               textTransform: "none",
